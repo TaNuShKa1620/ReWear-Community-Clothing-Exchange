2b3942c73fdeb618c66572a2d8d829af96d79c31
hottymithaulia <60284412+hottymithaulia@users.noreply.github.com>
2025-07-12 16:48:45 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    MONGO_URI = os.getenv("MONGO_URI")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    DEBUG = os.getenv("FLASK_DEBUG", "False") == "True"
